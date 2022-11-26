d275ba4920db2aedae565a1d4697d1a4a28d822e
Maxim R. v. Onciul <maxim.ritter.von.onciul@gmail.com>
2022-11-26 10:42:14 +0100

    delete unused nvim config

--- roles/nvim/files/nvim/lua/auto_pairs_setup.lua
+++
@@ -1,4 +0,0 @@
-vim.g.AutoPairsFlyMode = 1
--- vim.g.AutoPairs = { '('=')', '['=']', '{'='}',"'"="'",'"'='"', "`"="`", '```'='```', '"""'='"""', "'''"="'''", '\w\zs<' = '>'}
-vim.g.AutoPairsShortcutBackInsert = "<c-b>"
--- vim.cmd('source $HOME/.config/nvim/config/auto_pairs.vim')
