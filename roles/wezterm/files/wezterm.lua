1f726e82516e4c2c6e657f243bba547dd7e29112
Maxim R. v. Onciul <maxim.ritter.von.onciul@gmail.com>
2023-01-10 07:44:13 +0100

    wezterm role

---
+++ roles/wezterm/files/wezterm.lua
@@ -0,0 +1,48 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("mononoki"),
+	keys = {
+		{ key = "+", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+		{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+		{ key = "=", mods = "CTRL", action = wezterm.action.ResetFontSize },
+		{ key = "%", mods = "SUPER|SHIFT", action = wezterm.action.SplitHorizontal({ cwd = "~" }) },
+		{ key = "!", mods = "SUPER|SHIFT", action = wezterm.action.SplitVertical({ cwd = "~" }) },
+		{ key = "c", mods = "SUPER", action = "ActivateCopyMode" },
+		-- { key = "t", mods = "SUPER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	},
+	default_prog = { "/usr/bin/nu" },
+	-- color_scheme = "Gruvbox dark, hard (base16)",
+	enable_tab_bar = false,
+	cursor_blink_rate = 0,
+	colors = {
+		foreground = "#ebdbb2",
+		background = "#161616",
+		cursor_fg = "#161616",
+		cursor_bg = "#ebdbb2",
+		cursor_border = "#282828",
+		selection_bg = "#98971a",
+		selection_fg = "#161616",
+		compose_cursor = "#d65d0e",
+		ansi = {
+			"#0d0e0f",
+			"#cc241d",
+			"#98971a",
+			"#EEBD35",
+			"#458588",
+			"#b16286",
+			"#689d6a",
+			"#928374",
+		},
+		brights = {
+			"#928374",
+			"#fb4934",
+			"#b8bb26",
+			"#fabd2f",
+			"#7fa2ac",
+			"#D4879C",
+			"#8ec07c",
+			"#ebdbb2",
+		},
+	},
+}
