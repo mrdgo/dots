8f8fc8b8977fb5f01bc36d70542a72a86c32f74d
Maxim R. v. Onciul <maxim.ritter.von.onciul@gmail.com>
2023-05-23 12:00:11 +0200

    update wezterm config

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -1,49 +1,61 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
 
-return {
-	font = wezterm.font("mononoki"),
-	audible_bell = "Disabled",
-	keys = {
-		{ key = "+", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-		{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-		{ key = "=", mods = "CTRL", action = wezterm.action.ResetFontSize },
-		{ key = "%", mods = "SUPER|SHIFT", action = wezterm.action.SplitHorizontal({ cwd = "~" }) },
-		{ key = "!", mods = "SUPER|SHIFT", action = wezterm.action.SplitVertical({ cwd = "~" }) },
-		{ key = "c", mods = "SUPER", action = "ActivateCopyMode" },
-		-- { key = "t", mods = "SUPER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("Mononoki Nerd Font")
+config.font_size = 14
+config.audible_bell = "Disabled"
+config.keys = {
+	{ key = "+", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
+	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
+	{ key = "=", mods = "CTRL", action = wezterm.action.ResetFontSize },
+	{ key = "%", mods = "SUPER|SHIFT", action = wezterm.action.SplitHorizontal({ cwd = "~" }) },
+	{ key = "!", mods = "SUPER|SHIFT", action = wezterm.action.SplitVertical({ cwd = "~" }) },
+	{ key = "c", mods = "SUPER", action = "ActivateCopyMode" },
+	-- { key = "t", mods = "SUPER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+}
+
+config.default_prog = { "/usr/bin/nu" }
+
+config.enable_tab_bar = false
+config.cursor_blink_rate = 0
+-- color_scheme = "Gruvbox dark, hard (base16)"
+config.colors = {
+	foreground = "#ebdbb2",
+	background = "#161616",
+	cursor_fg = "#161616",
+	cursor_bg = "#ebdbb2",
+	cursor_border = "#282828",
+	selection_bg = "#98971a",
+	selection_fg = "#161616",
+	compose_cursor = "#d65d0e",
+	ansi = {
+		"#0d0e0f",
+		"#cc241d",
+		"#98971a",
+		"#EEBD35",
+		"#458588",
+		"#b16286",
+		"#689d6a",
+		"#928374",
 	},
-	default_prog = { "/usr/bin/nu" },
-	-- color_scheme = "Gruvbox dark, hard (base16)",
-	enable_tab_bar = false,
-	cursor_blink_rate = 0,
-	colors = {
-		foreground = "#ebdbb2",
-		background = "#161616",
-		cursor_fg = "#161616",
-		cursor_bg = "#ebdbb2",
-		cursor_border = "#282828",
-		selection_bg = "#98971a",
-		selection_fg = "#161616",
-		compose_cursor = "#d65d0e",
-		ansi = {
-			"#0d0e0f",
-			"#cc241d",
-			"#98971a",
-			"#EEBD35",
-			"#458588",
-			"#b16286",
-			"#689d6a",
-			"#928374",
-		},
-		brights = {
-			"#928374",
-			"#fb4934",
-			"#b8bb26",
-			"#fabd2f",
-			"#7fa2ac",
-			"#D4879C",
-			"#8ec07c",
-			"#ebdbb2",
-		},
+	brights = {
+		"#928374",
+		"#fb4934",
+		"#b8bb26",
+		"#fabd2f",
+		"#7fa2ac",
+		"#D4879C",
+		"#8ec07c",
+		"#ebdbb2",
 	},
 }
+
+return config
